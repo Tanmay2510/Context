b11e7798d8aa590e31c6f88e2525e0ec6f0c16df
Tanmay2510 <comparisionthing@gmail.com>
2023-01-20 01:34:08 +0530

    Started learning context api

---
+++ src/components/Booklist.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Booklist() {
+  return (
+    <div className='book-list'>
+    <ul>
+    <li> The way of kings</li>
+    <li> The way of Queens</li>
+    <li> The way of People</li>
+    </ul>
+    </div>
+  )
+}
+
+export default Booklist
